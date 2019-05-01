713e7bd59aa42e02ba305670cede62aa3561d35e
pasudo123 <pasudo123@naver.com>
2019-05-01 22:46:33 +0900

    jpa auditing 및 boolean type converter 추가

---
+++ src/main/java/edu/doubler/app/config/BooleanToYNConverter.java
@@ -0,0 +1,19 @@
+package edu.doubler.app.config;
+
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter(autoApply = true)
+public class BooleanToYNConverter implements AttributeConverter<Boolean, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Boolean attribute) {
+        return (attribute != null && attribute) ? "Y" : "N";
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(String dbData) {
+        return "Y".equals(dbData);
+    }
+}
